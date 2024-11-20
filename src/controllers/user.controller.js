b328270fd2dfd1278e4cde4a2d188a2908fe4224
Gadhave Om Sanjay <gadhave.omibaba27@gmail.com>
2024-11-20 17:31:07 +0530

    Add getWatchHistory endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -274,8 +274,6 @@ const updateUserAvatar = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, user, "Avatar uploaded successfully"));
 });
 
-
-
 const getUserChannelProfile = asyncHandler(async(req, res) => {
     const {username} = req.params
     if (!username?.trim()) {
@@ -343,7 +341,59 @@ const getUserChannelProfile = asyncHandler(async(req, res) => {
     )
 })
 
+const getWatchedHistory = asyncHandler(async (req, res) => {
+    
+  const user = await User.aggregate([
+    {
+      $match : {
+        _id: mongoose.Types.ObjectId(req.user._id)
+      }
+    },
+    {
+      $lookup : {
+        from : "videos",
+        localField : "watchHistory",
+        foreignField : "_id",
+        as : "watchedHistory",
+        pipeline : [
+          {
+              $lookup : "users",
+              localField : "owner",
+              foreignField : "_id",
+              as : "owner",
+              pipeline : [
+                {
+                  $project : {
+                    fullName: 1,
+                    username: 1,
+                    avatar: 1
+                  }
+                }
+              ]
+          },
+          {
+            $addFields : {
+              owner : {
+                $first : "$owner"
+              }
+            }
+          }
+        ]
+      }
+    }
+  ])
 
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(
+      200,
+      user[0].watchedHistory,
+      "Watch History fetch successfully"
+    )
+  )
+
+});
 
 
 export {
@@ -357,4 +407,5 @@ export {
   updateUserAvatar,
   updateUserCoverImage,
   getUserChannelProfile,
+  getWatchedHistory
 };
